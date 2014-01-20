d381082ac83874a67d36575aebacb9157c7018ba
Andrew Petersen <kirbysayshi@gmail.com>
2014-01-20 01:27:25 -0500

    wooo overengineering

---
+++ README.md
@@ -0,0 +1,31 @@
+tap-browser-color
+=================
+
+A super simple tap/tape reporter that changes the body tag background color depending on if the tests are passing, failing, or pending.
+
+This is meant for live-coding or environments where you want a big swath of color when your test status changes.
+
+Usage
+=====
+
+Best used with [requirebin](http://requirebin.com/)...
+
+```js
+// Executing immediately intercepts (and still passes) console.log output
+var undo = require('tap-browser-color')();
+var tape = require('tape');
+
+test('yep', function(t) {
+  t.ok('whatup');
+  t.end();
+});
+
+// Entirely optional, this just removes the `console.log` intercept.
+// Probably pretty useless.
+undo();
+```
+
+License
+=======
+
+MIT
---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * Activate the runner by overriding `console.log` to intercept tap output.
+ * Call the return value to undo the override.
+ */
+
+module.exports = function() {
+  var olog = console.log;
+  var pre = document.body.appendChild(document.createElement('pre'));
+  console.log = function(line) {
+    parse(line);
+    style();
+    olog.apply(console, arguments);
+    pre.innerHTML += line + '\n';
+  }
+
+  return function undo() {
+    console.log = olog;
+  }
+}
+
+/**
+ * These control what colors are used for the pending/failing/passing states.
+ * Ensure that these are assigned by individual value, and that the entire
+ * object is not assigned at once to ensure that references are intact.
+ */
+
+var colors = module.exports.colors = {
+    PENDING: '#FCD62A'
+  , FAILING: '#F28E82'
+  , PASSING: '#8ECA6C'
+}
+
+var failed = 0;
+var passed = 0;
+var finish = false;
+
+function parse(line) {
+  if (line.indexOf('ok') === 0) {
+    passed += 1;
+    return;
+  }
+
+  if (line.indexOf('not ok') === 0) {
+    failed += 1;
+    return;
+  }
+}
+
+function style() {
+  var s = document.body.style;
+  if (failed > 0) {
+    s.backgroundColor = colors.FAILING;
+  } else if (passed > 0 && failed === 0) {
+    s.backgroundColor = colors.PASSING;
+  } else {
+    s.backgroundColor = colors.PENDING;
+  }
+}
